a595f268dd86c3c148f9e4fe8d3ef68409d4dc8b
神奇辉 <shenqihui0920@gmail.com>
2015-06-29 22:17:00 +0800

    提交初步可用版本。

--- index.js
+++ index.js
@@ -8,62 +8,62 @@ var JsonAllReporter = function(baseReporterDecorator, config, logger, helper, fo
   var reporterConfig = config.jsonAllReporter || {};
   var outputFile = helper.normalizeWinPath(path.resolve(config.basePath, reporterConfig.outputFile || 'json-all-results.json'));
 
+  var suites = {};
+
+  function buildSuite(browser, type, value) {
+    // debugger;
+    suites[browser.id] = suites[browser.id] || {};
+    suites[browser.id]['id'] = suites[browser.id]['id'] || browser.name || '';
+    suites[browser.id]['name'] = suites[browser.id]['name'] || browser.name || '';
+    suites[browser.id]['fullname'] = suites[browser.id]['fullname'] || browser.name || '';
+    suites[browser.id][type] = suites[browser.id][type] || [];
+    suites[browser.id][type].push(value);
+  }
+
 
   this.onRunStart = function(browsers) {
-    console.log(arguments);
+    // debugger;
   };
-
-  this.onBrowserStart = function(browser) {
-    console.log(arguments);
-  };
-
-  this.renderBrowser = function (browser) {
-    console.log(arguments);
-  };
-
-  this.renderBrowser = this.renderBrowser.bind(this);
 
   this.write = function () {
-    console.log(arguments);
+    // debugger;
   };
 
   this.writeCommonMsg = this.write;
 
   this.onBrowserError = function (browser, error) {
-    console.log(arguments);
+    buildSuite(browser, 'error', error);
   };
 
   this.onBrowserLog = function (browser, log, type) {
-    console.log(arguments);
+    buildSuite(browser, 'log', log);
   };
 
   this.onBrowserComplete = function(browser) {
-    console.log(arguments);
+    // debugger;
   };
 
   this.onSpecComplete = function (browser, result) {
-    console.log(arguments);
-  };
-
-  this.specSuccess = this.specSkipped = function () {
-    console.log(arguments);
-  };
-
-  this.specFailure = function (browser, result) {
-    console.log(arguments);
+    // debugger;
   };
 
   this.onRunComplete = function() {
-    console.log(arguments);
+    // debugger;
+    console.log(suites);
+    var writeStr = JSON.stringify(suites, null, 4);
+    var fd = fs.openSync(outputFile, 'w+');
+    fs.writeSync(fd, writeStr, 0, "utf8");
+    fs.closeSync(fd);
+    suites = {}
   };
 
   this.specSuccess = this.specSkipped = this.specFailure = function(browser, result) {
-    console.log(arguments);
+    // debugger;
   };
 
-  // wait for writing all the xml files, before exiting
   this.onExit = function(done) {
-    console.log(arguments);
+    suites.karma = done;
+    // debugger;
   };
 };
 
